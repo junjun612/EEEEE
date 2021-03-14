de35fd5fcfdd400b055a72d6ae4f7852d1be6a96
junjun612 <junjun612@naver.com>
2021-03-14 16:54:25 +0900

    * xcode 커맨드라인 기본 프로젝트 추가

---
+++ EEEEE/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EEEEE
+//
+//  Created by 김민준 on 2021/03/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
